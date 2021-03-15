9027c2ce1d2ccae21f592821760ca71f2adbe96b
kubasulek2 <kubasulek2@gmail.com>
2021-03-15 14:16:47 +0100

    Chars and Strings

--- integers.cpp
+++ integers.cpp
@@ -1,57 +1,48 @@
 #include <iostream>
-#include <climits>
+/* Only then strings will work */
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
 
 
 
 
 
-// In every program there must be function called main which will be automatically called when process runs
 int main()
 {
-	/* Signed integers */
+	
+	// char uses single quote
+	char x = 'A';
+	// char is actually integer, range -128 to 127 
+	char y = 65;
+	
+	// char is actually integer, range 0 to 255 
+	unsigned char z = 131;
 
-	// signed 2 bytes integer
-	short x;
 
-	// signed 2 or 4 bytes integer (depends on system)
-	int y;
-	
-	// signed  4 bytes integer
-	long z = 1234757573;
-	
-	// signed  8 bytes integer
-	long long q = 1234757573;
-	
+	cout << x << " = " << y << endl;
+	// cast char as int
+	cout << (int) z << endl;
+	cout << "z " << z << endl;
 
-	/* Unsigned integers */
+	// 1 byte
+	cout << "size of char " << sizeof(x) << endl;
 
-	// unsigned 2 bytes integer
-	unsigned short a;
+	/* Strings */
+	
+	string str = "";
+	
+	/* Old C way of terminating strings */
+	string C_style = "example\0";
+		
+	cout << str << endl;
 
-	// unsigned 2 or 4 bytes integer (depends on system)
-	unsigned int b;
-	
-	// unsigned  4 bytes integer
-	unsigned long c = 1234757573;
-	
-	// unsigned  8 bytes integer
-	unsigned long long d = 1234757573;
-	
-	// all integers if not initialized are autmatically initialized with zero value, booleans the same
-	cout << y << endl;
+	cout << "size of str " << sizeof(str) << endl;
 
-	// sizeof function
-	
-	cout << "size of int " << sizeof(b) << endl;
 
-	/* limitc constants */
-	cout << SHRT_MIN << endl;
-	cout << SHRT_MAX << endl;
-	cout << USHRT_MAX << endl;
-	cout << UINT64_MAX << endl;
-	
+
+
 	return EXIT_SUCCESS;
 }
